d76a2c6ad4ba3eedbd12b9be52308c56a01c6eff
Dimasik <prevetek@gmail.com>
2016-09-27 00:19:09 +0300

    v 1.0.b
     Все работает, багов не обнаружено.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 4_json
+
+Скрипт предназначен для вывода содержимого json файлов в формате удобном для чтения, называемом **pretty print**
+
+correct.json - правильный json файл
+encorrect.txt - json файл с ошибкой
+
+**run script**
+$ python3 pprint_json.py "_file name_"
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    f = open(filepath, "r")
+    data = f.read()
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    parser = json.loads(data)
+    pretty = json.dumps(parser, ensure_ascii=False, indent=4, sort_keys=True)
+    return pretty
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        print("Укажите путь к json файлу")
+    else:
+        if len(sys.argv) >= 3:
+            print("Много параметров, необходимо указать только путь к json файлу")
+        else:
+            try:
+                data = load_data(sys.argv[1])
+                print(pretty_print_json(data))
+            except FileNotFoundError:
+                print("Указанного файла не существует")
+            except json.decoder.JSONDecodeError:
+                print("Указанный фаил поврежден")
+
+
+
